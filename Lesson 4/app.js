5de78658ab29ae21a2f376eea21e842a12ca0f88
GitHubcodemyster <riocodingexpert@gmail.com>
2019-01-26 17:57:08 +0800

    project 3

--- Lesson 4/app.js
+++ Lesson 4/app.js
@@ -5,11 +5,21 @@ var ctx = c.getContext("2d");
 var img = new Image();
 img.src = "tree.png"
 
+var img2 = new Image();
+img.src = "tree.png"
+
+var img3 = new Image();
+img.src = "tree.png"
+
 img.onload = function(){
+	ctx.drawImage(img, 150, 150, 100,100);
+}
+img2.onload = function(){
 	ctx.drawImage(img, 100, 100, 100,100);
 }
-ctx.fillStyle = "red"
-ctx.strokeStyle = "black"
-ctx.font = "30px Aerial"
-ctx.fillText("hi",100, 100);
-ctx.strokeText("hi", 100, 100)
+img3.onload = function(){
+	ctx.drawImage(img, 200, 200, 100,100);
+}
+ctx.font = "70px Impact"
+ctx.fillText("hi",150, 40);
+ctx.strokeText("hi", 150, 40)
